bec03a9622cb42a199f1f64c301968ee1df7a310
DHARANI P <119476559+dhar127@users.noreply.github.com>
2024-01-18 08:57:39 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (52.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        
+        int x=1,y=1,t=0;
+        for(int i=2;i<=n;i++){
+            t=x+y;
+            y=x;
+            x=t;
+        }
+        return t;
+    }
+}
