55c47212c822e6fede7a86639d1a7c59b6a65322
DHARANI P <119476559+dhar127@users.noreply.github.com>
2024-03-19 21:06:48 +0530

    Time: 357 ms (84.27%), Space: 17.1 MB (79.06%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,14 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+       
+        task_counts = Counter(tasks)
+        max_count = max(task_counts.values())
+        
+        max_count_tasks = sum(1 for count in task_counts.values() if count == max_count)
+        
+       
+        intervals_needed = (max_count - 1) * (n + 1) + max_count_tasks
+        
+        
+        return max(intervals_needed, len(tasks))
+        
