003c95e7f87909ee9410387389ec7f7bd017fd31
DHARANI P <119476559+dhar127@users.noreply.github.com>
2024-04-05 19:06:28 +0530

    Time: 42 ms (15.32%), Space: 16.5 MB (75.32%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brackets = {'(': ')', '[': ']', '{': '}'}
+        for char in s:
+            if char in brackets.keys():
+                stack.append(char)
+            elif char in brackets.values():
+                if not stack or char != brackets[stack.pop()]:
+                    return False
+        print(stack)
+        return len(stack) == 0
